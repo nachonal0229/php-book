7a8c5659c7d6d8f754cde8104a5e75fa119df4ea
nachonal0229 <nachonal0229@gmail.com>
2019-09-25 23:42:09 +0900

    first commit

---
+++ app/src/Controller/AppController.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     0.2.9
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Controller;
+
+use Cake\Controller\Controller;
+
+/**
+ * Application Controller
+ *
+ * Add your application-wide methods in the class below, your controllers
+ * will inherit them.
+ *
+ * @link https://book.cakephp.org/3.0/en/controllers.html#the-app-controller
+ */
+class AppController extends Controller
+{
+    /**
+     * 初期化処理
+     * 
+     * @return void
+     * @throws \Exception
+     */
+    public function initialize() {
+        parent::initialize();
+
+        $this->loadComponent('RequestHandler', ['enableBeforeRedirect' => false,]);
+
+        $this->loadComponent('Flash');
+
+        $this->loadComponent('Auth', [
+                'authenticate' => [ 
+                    'Form' => [
+                                    'feilds' => [
+                                        'username' => 'username',
+                                        'password' => 'password'
+                                    ]
+                            ]
+                ],
+                'loginAction' => [
+                    'controller' => 'Login',
+                    'action' => 'index'
+                ],
+                'loginRedirect' => [
+                    'controller' => 'Questions',
+                    'action' => 'index'
+                ],
+                'logoutRedirect' => [
+                    'controller' => 'Login',
+                    'action' => 'index'
+                ],
+                'unauthorizedRedirect' => [
+                    'controller' => 'Login',
+                    'action' => 'index'
+                ],
+                'authError' => 'ログインが必要です'
+        ]);
+
+        $this->Auth->allow(['display','index','view']);
+    }
+}
